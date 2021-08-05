4dfbdfc94346585cd29788c960f6ccd2325b5f46
Umid <umidjan.kushayev@gmail.com>
2021-08-05 15:13:57 +0600

    - bug fix, removed missing components

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,17 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Header from "../components/Header/Header";
-import Home from "../components/Main/Home";
-import Sidebar from "../components/Sidebar/Sidebar";
 
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Header />
-            <Sidebar />
-            <Switch>
-                <Route exact path="/" component={Home} />
-            </Switch>
+            <Switch></Switch>
         </BrowserRouter>
     );
 };
